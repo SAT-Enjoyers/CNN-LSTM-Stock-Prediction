57a5b87a3e5cfcfe0e2c0f10e006c07a533d823e
cdv123 <charles.duboisveltman@gmail.com>
2023-11-04 21:45:26 +0000

    clean up model.py code

--- model.py
+++ model.py
@@ -10,49 +10,51 @@ from keras.layers import Dropout
 
 
 def main():
-    print("h")
+    
     model = Sequential()
     cnn(model)
     lstm(model)
-    
     print('Loading...')
-    fileName = np.load('data_prep_out.npy')
-    all_labels = []
-    fileName = np.reshape(fileName,(58750,10,5))
-    # create labels
-    for i in range(0,len(fileName)-1):
-        if fileName[i][1][0] < fileName[i+1][1][-1]:
-            all_labels.append(1)
-        else:
-            all_labels.append(0)
-    for i in range(len(fileName),len(fileName)-1):
-        if fileName[i][1][-1] < fileName[i+1][1][-1]:
-            all_labels.append(1)
-        else:
-            all_labels.append(0)
-    all_labels.append(0)
-    test_labels = all_labels[47000-1:]
-    train_labels = all_labels[:-11751]
+
+    # load data
+    all_series = np.load('data_prep_out.npy')
+
+    # reshape to add labels
+    all_series = np.reshape(all_series,(len(all_series[0]),10,5))
+
+    # value of when test inputs start
+    test_index = 47000
+
+    # set random seed for consistent results
     np.random.seed(0)  
     # np.random.shuffle(fileName)
-    labels = np.load('labels.npy')
-    print(np.shape(fileName))
-    file_chuncks = np.split(fileName,5,axis=1)
+
+    # splits
+    series_chunks = np.split(all_series,5,axis=1)
     train_series = []
+
     for i in range(4):
-        train_series.append(file_chuncks[i])
-    test_series = file_chuncks[-1]
-    train_series = np.reshape(np.array(train_series),(47000,10,5))
-    train_labels.append(0)
-    print('Train...')
+        train_series.append(series_chunks[i])
+        
+    test_series = series_chunks[-1]
+    train_series = np.reshape(np.array(train_series),(test_index,10,5))
+
+    #create labels
+    test_labels,train_labels = create_all_labels(all_series,test_index)
+
+    # reshape for training
     y_train = np.array(train_labels)
-    y_train = y_train.reshape(47000)
-    test_series = np.reshape(test_series,(11750,10,5))
+    y_train = y_train.reshape(test_index)
+    test_series = np.reshape(np.array(test_series),(len(all_series)-test_index,10,5))
     test_labels = np.array(test_labels)
     y_test = np.array(test_labels)
+
+    #train model
     model.fit(train_series, y_train,
             batch_size=64,
-            epochs=10,)
+            epochs=15,)
+    
+    #evaluate model
     score,acc = model.evaluate(test_series,y_test)
     print(score,acc)
     model.save("model.keras")
@@ -71,29 +73,38 @@ def cnn(model):
 #batch_size = number of samples in each batch
 #timesteps = The number of time steps or sequences you want to consider. This could be the length of the time series data you want to feed into the LSTM.
 #features = The number of features extracted from CNN layer
+
 def lstm(model):
 
-
     model.add(LSTM(64))
-    model.add(Dropout(.5))
+    # model.add(Dropout(.5))
     model.add(Dense(1,activation='sigmoid'))
     optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
 
     model.compile(loss='binary_crossentropy',
                 optimizer=optimizer,
                 metrics=['accuracy'])
-    
-    #score, acc = model.evaluate(x_test, y_test, batch_size=64)
-    # lstm = tf.keras.layers.LSTM(64)
-    # output = lstm(inputTensor)
-    # model.add(LSTM(64))
-    # model.add(Dense(10, activation='sigmoid'))
 
-    # # Compile the model
-    # model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-    # Make predictions with the model
-    # predictions = model.predict(inputTensor[:3])
-    # print(f'Predictions: {predictions}')
+def create_all_labels(all_series,test_index):
+    all_labels = []
+    # create labels
+    #
+    for i in range(0,test_index):
+        # checks if series is increasing
+        if all_series[i][1][0] < all_series[i+1][1][-1]:
+            all_labels.append(1)
+        else:
+            all_labels.append(0)
+    for i in range(test_index,len(all_series)-1):
+        # check if stock should have been bought
+        if all_series[i][1][-1] < all_series[i+1][1][0]:
+            all_labels.append(1)
+        else:
+            all_labels.append(0)
+    all_labels.append(0)
+    test_labels = all_labels[test_index:]
+    train_labels = all_labels[:test_index-len(all_series)]
+    return test_labels, train_labels
 
 main()
+
