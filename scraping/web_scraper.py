df7cfd9e03a221410f90f497d98bd2cfcf71fd4b
Michal Pluta <vsdc48@durham.ac.uk>
2023-11-04 23:27:06 +0000

    Improve validaton of web scraper and add more data

--- scraping/web_scraper.py
+++ scraping/web_scraper.py
@@ -4,7 +4,12 @@ import requests as rq
 import pandas as pd
 import numpy as np
 
+# GLOBAL VARIABLES
 FILEPATH = 'scraping/'
+OUT_FILEPATH = FILEPATH + 'SP_data_full.csv'
+startDate = '04/03/1957'
+endDate = 'today' # actual date or 'today'
+numStocks = 503 # current max = 503
 
 # String to unix timestamp
 def getTimestamp(strDate: str) -> int:
@@ -36,13 +41,11 @@ def add_label(df):
     df['label'] = np.where(condition, 0, 1)
     df.at[0, 'label'] = 0
 
-# Scraping variables
-startDate = getTimestamp('01/11/2018')
-endDate = getTimestamp('today') # actual date or 'today'
-numStocks = 40 # current max = 503
-
 # Initial DataFrame
 df = pd.DataFrame([], columns=['date', 'open', 'high', 'low', 'close', 'volume'], dtype = str)
+
+startDate = getTimestamp(startDate)
+endDate = getTimestamp(endDate)
 
 # Scrape all the stocks
 for idx, stock_tag in enumerate(getAllTags()[:numStocks]):
@@ -53,28 +56,28 @@ for idx, stock_tag in enumerate(getAllTags()[:numStocks]):
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
     r = rq.get(url=my_url, headers=headers)
 
-    # Get raw data from request
-    data = r.text
-    data = data.split("\n")
-    data = [day.split(",") for day in data][1:]
+    if r.status_code == 200:
+        # Get raw data from request
+        data = r.text
+        data = data.split("\n")
+        data = [day.split(",") for day in data][1:]
 
-    # Convert raw data to DataFrame
-    temp_df = pd.DataFrame(data, columns =['date', 'open', 'high', 'low', 'close', 'adj_close', 'volume'], dtype = str)
+        
+        # Convert raw data to DataFrame
+        temp_df = pd.DataFrame(data, columns =['date', 'open', 'high', 'low', 'close', 'adj_close', 'volume'], dtype = str)
+        
+        # Drop adjusted close column and add stock tag to the rows
+        temp_df = temp_df.drop(['adj_close'], axis=1)
+        temp_df['name'] = stock_tag
+
+        # Add labels for closing price
+        add_label(temp_df)
+
+        # Add rows from stock to the main DataFrame
+        df = pd.concat([df, temp_df], ignore_index=True)
     
-    # Drop adjusted close column and add stock tag to the rows
-    temp_df = temp_df.drop(['adj_close'], axis=1)
-    temp_df['name'] = stock_tag
-
-    # Add labels for closing price
-    add_label(temp_df)
-
-    # Add rows from stock to the main DataFrame
-    df = pd.concat([df, temp_df], ignore_index=True)
-    
-
 # Convert format of dates
-df['date'] = df['date'].apply(changeDate)
-    
+df['date'] = df['date'].apply(changeDate)   
 
 # Output scraped data to csv file
-df.to_csv(FILEPATH + 'output_data.csv', index=False)
+df.to_csv(OUT_FILEPATH, index=False)
