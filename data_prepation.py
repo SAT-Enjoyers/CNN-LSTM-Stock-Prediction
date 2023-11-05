8f735ec0e8a3dc8f870a427ea35c394199055b17
Michal Pluta <vsdc48@durham.ac.uk>
2023-11-05 08:12:49 +0000

    Collect labels from data in data prep

--- data_prepation.py
+++ data_prepation.py
@@ -3,19 +3,24 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 import matplotlib.pyplot as plt
 
-DATASET_FILEPATH = 'scraping/SP_data_test.csv'
+DATASET_FILEPATH = 'scraping/SP_data.csv'
 
 def get_all_time_series(df):
     unique_names = df["name"].unique()
     ts_open, ts_high, ts_low, ts_close, ts_vol = [], [], [], [], []
-    
+    df_labels = {}
+
     for name in unique_names:
+        
         # Get all rows belonging to a stock
         tsRows = df[df['name'] == name]
 
         # Ensure the number of rows is a multiple of 10
         ts_length = tsRows.shape[0]
-        adjusted_length = ts_length - ts_length % 10
+
+        actual_partitions = (ts_length - 1) // 10
+        adjusted_length = actual_partitions * 10 + 1
+
 
         # Extract time series from rows
         df_open = normalise_time_series(tsRows['open'][:adjusted_length])
@@ -24,14 +29,21 @@ def get_all_time_series(df):
         df_close = normalise_time_series(tsRows['close'][:adjusted_length])
         df_vol = normalise_time_series(tsRows['volume'][:adjusted_length])
 
-        # Partition each time series into chunks of 10
-        partitionTimeSeries(df_open, ts_open)
-        partitionTimeSeries(df_high, ts_high)
-        partitionTimeSeries(df_low, ts_low)
-        partitionTimeSeries(df_close, ts_close)
-        partitionTimeSeries(df_vol, ts_vol)
+        df_close = df_close.reset_index(drop=True)
 
-    return [ts_open, ts_high, ts_low, ts_close, ts_vol]
+        df_labels[name] = [df_close[10 * i] for i in range(1, actual_partitions + 1)]
+
+        # Partition each time series into chunks of 10
+        partitionTimeSeries(df_open[:-1], ts_open)
+        partitionTimeSeries(df_high[:-1], ts_high)
+        partitionTimeSeries(df_low[:-1], ts_low)
+        partitionTimeSeries(df_close[:-1], ts_close)
+        partitionTimeSeries(df_vol[:-1], ts_vol)
+
+    print(sum(len(data) for data in df_labels.values()))
+    print(len(ts_close))
+
+    return ([ts_open, ts_high, ts_low, ts_close, ts_vol], df_labels)
 
 # Receives an un-normalised unsplit time series as input
 # (datapoint - average) / standard deviation
@@ -49,9 +61,10 @@ def partitionTimeSeries(ts_unsplit, ts_split):
 
 
 df = pd.read_csv(DATASET_FILEPATH)
-all_time_series = get_all_time_series(df)
+all_time_series, labels = get_all_time_series(df)
 
 all_time_series = np.array(all_time_series)
 print(np.shape(all_time_series))
 
 np.save('data_prep_out', all_time_series)
+np.save('data_prep_out_labels', labels)
