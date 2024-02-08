92ea0cbd80013c39b6f414ffaac5703ec97c40d7
Michal Pluta <vsdc48@durham.ac.uk>
2024-02-08 18:09:36 +0000

    Refactor more of the data preparation and format the code

--- data_prepation.py
+++ data_prepation.py
@@ -1,9 +1,8 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt 
-import matplotlib.pyplot as plt
 
 DATASET_FILEPATH = 'scraping/SP_data.csv'
+
 
 def get_all_time_series(df):
     unique_names = df["name"].unique()
@@ -11,34 +10,34 @@ def get_all_time_series(df):
     df_labels = []
 
     for name in unique_names:
-        
+        print(name)
+
         # Get all rows belonging to a stock
         tsRows = df[df['name'] == name]
 
-        # Ensure the number of rows is a multiple of 10
-        ts_length = tsRows.shape[0]
-
-        actual_partitions = (ts_length - 1) // 10
+        actual_partitions = (tsRows.shape[0] - 1) // 10
         adjusted_length = actual_partitions * 10 + 1
 
+        tsRows = tsRows[:adjusted_length]
 
         # Extract time series from rows
-        df_open = normalise_time_series(tsRows['open'][:adjusted_length])
-        df_high = normalise_time_series(tsRows['high'][:adjusted_length])
-        df_low = normalise_time_series(tsRows['low'][:adjusted_length])
-        df_close = normalise_time_series(tsRows['close'][:adjusted_length])
-        df_vol = normalise_time_series(tsRows['volume'][:adjusted_length])
+        df_open = normalise_time_series(tsRows['open'])
+        df_high = normalise_time_series(tsRows['high'])
+        df_low = normalise_time_series(tsRows['low'])
+        df_close = normalise_time_series(tsRows['close'])
+        df_vol = normalise_time_series(tsRows['volume'])
 
         df_close = df_close.reset_index(drop=True)
 
-        df_labels.extend([df_close[10 * i] for i in range(1, actual_partitions + 1)])
+        df_labels.extend([df_close[10 * i]
+                         for i in range(1, actual_partitions + 1)])
 
         # Partition each time series into chunks of 10
         partitionTimeSeries(df_open[:-1], ts_open)
         partitionTimeSeries(df_high[:-1], ts_high)
         partitionTimeSeries(df_low[:-1], ts_low)
         partitionTimeSeries(df_close[:-1], ts_close)
-        partitionTimeSeries(df_vol[:-1], ts_vol)   
+        partitionTimeSeries(df_vol[:-1], ts_vol)
 
     return ([ts_open, ts_high, ts_low, ts_close, ts_vol], df_labels)
 
@@ -57,11 +56,12 @@ def partitionTimeSeries(ts_unsplit, ts_split):
         ts_split.append(partition)
 
 
-df = pd.read_csv(DATASET_FILEPATH)
-all_time_series, labels = get_all_time_series(df)
+if __name__ == '__main__':
+    df = pd.read_csv(DATASET_FILEPATH)
+    all_time_series, labels = get_all_time_series(df)
 
-all_time_series = np.array(all_time_series)
-print(np.shape(all_time_series))
+    all_time_series = np.array(all_time_series)
+    print(np.shape(all_time_series))
 
-np.save('data_prep_out', all_time_series)
-np.save('data_prep_out_labels', labels)
+    np.save('data_prep_out', all_time_series)
+    np.save('data_prep_out_labels', labels)
--- model.py
+++ model.py
@@ -1,16 +1,11 @@
 import tensorflow as tf
 import numpy as np
-from keras.layers import Dense, LSTM
-from keras.layers import Conv1D
 from keras.models import Sequential
-from keras.layers import MaxPooling1D
-from keras.layers import TimeDistributed
-from keras.layers import Flatten
-from keras.layers import Dropout
+from keras.layers import Conv1D, MaxPooling1D, Dense, LSTM, TimeDistributed, Flatten, Dropout
 
 
 def main():
-    
+
     model = Sequential()
     cnn(model)
     lstm(model)
@@ -21,7 +16,7 @@ def main():
 
     print("Reshape series")
     print(np.shape(all_series))
-    all_series = np.reshape(all_series,(len(all_series[0]),10,5))
+    all_series = np.reshape(all_series, (len(all_series[0]), 10, 5))
     print(np.shape(all_series), "\n")
 
     # value of when test inputs start
@@ -37,81 +32,84 @@ def main():
     print("Train shape: ", np.shape(train_series))
     print("Test shape: ", np.shape(test_series))
 
-    #create labels  
-    test_labels, train_labels = create_all_labels(all_series,test_index)
+    # create labels
+    test_labels, train_labels = create_all_labels(all_series, test_index)
 
     # reshape for training
     y_train = np.array(train_labels)
     y_train = y_train.reshape(test_index)
-    test_series = np.reshape(np.array(test_series),(len(all_series)-test_index,10,5))
+    test_series = np.reshape(np.array(test_series),
+                             (len(all_series)-test_index, 10, 5))
     test_labels = np.array(test_labels)
     y_test = np.array(test_labels)
 
-    #train model
+    # train model
     model.fit(train_series, y_train,
-            batch_size=64,
-            epochs=10,)
+              batch_size=64,
+              epochs=10,)
 
-    
     # get resuts
     correct_cnt = 0
     results = model.predict(train_series)
     print(len(results))
-    for index,result in enumerate(results):
+    for index, result in enumerate(results):
         if y_train[index] < train_series[index][-1][3] and result < train_series[index][-1][3]:
-            correct_cnt +=1
+            correct_cnt += 1
         elif y_train[index] > train_series[index][-1][3] and result > train_series[index][-1][3]:
-            correct_cnt +=1
+            correct_cnt += 1
     accuracy = correct_cnt/len(results)
     print(accuracy)
     correct_cnt = 0
     results = model.predict(test_series)
-    print(len(results),len(y_test),len(test_series[0][-1]))
-    for index,result in enumerate(results):
+    print(len(results), len(y_test), len(test_series[0][-1]))
+    for index, result in enumerate(results):
         if (y_test[index] < test_series[index][-1][3]) and (result[0] < test_series[index][-1][3]):
-            correct_cnt +=1
+            correct_cnt += 1
         elif (y_test[index] > test_series[index][-1][3]) and (result[0] > test_series[index][-1][3]):
-            correct_cnt +=1
+            correct_cnt += 1
     accuracy = correct_cnt/len(results)
     print(accuracy)
     # for i in range(len(results)):
     #     if test_series[i][-1][3]:
     #         print(results[i],y_test[i],test_series[i][-1][3])
     # print(accuracy)
-    score,mae = model.evaluate(test_series,y_test)
-    print(score,mae)
-    
+    score, mae = model.evaluate(test_series, y_test)
+    print(score, mae)
+
+
 def cnn(model):
-    sides = 5 # Size of sides
-    channels = 1 # Grayscale
-    timeStep = 10 # Timestep
+    sides = 5  # Size of sides
+    channels = 1  # Grayscale
+    timeStep = 10  # Timestep
     input_shape = (timeStep, sides)
-    
+
     # model.add(TimeDistributed(Conv1D(filters=32, kernel_size=1, strides=5, activation='tanh'), input_shape=input_shape))
     # model.add(TimeDistributed(MaxPooling1D(pool_size=1)))
     model.add(Conv1D(filters=32, kernel_size=1, strides=5, activation='tanh'))
     model.add(MaxPooling1D(pool_size=1))
 # From here onwards, just CNN
-#Shape should be (batch_size, timesteps, features)
-#batch_size = number of samples in each batch
-#timesteps = The number of time steps or sequences you want to consider. This could be the length of the time series data you want to feed into the LSTM.
-#features = The number of features extracted from CNN layer
+# Shape should be (batch_size, timesteps, features)
+# batch_size = number of samples in each batch
+# timesteps = The number of time steps or sequences you want to consider. This could be the length of the time series data you want to feed into the LSTM.
+# features = The number of features extracted from CNN layer
+
 
 def lstm(model):
 
-    model.add(LSTM(units= 64, input_shape = (10,32)))
+    model.add(LSTM(units=64, input_shape=(10, 32)))
     model.add(Dropout(.1))
-    model.add(Dense(1,activation='tanh'))
+    model.add(Dense(1, activation='tanh'))
     optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
     model.compile(loss='mean_absolute_error',
-                optimizer=optimizer,
-                metrics=['mae'])
+                  optimizer=optimizer,
+                  metrics=['mae'])
 
-def create_all_labels(all_series,test_index):
 
-    all_labels = [] 
+def create_all_labels(all_series, test_index):
+
+    all_labels = []
     # create labels
-    for i in range(0,len(all_series)-1):
+    for i in range(0, len(all_series)-1):
         # check if stock should have been bought
         all_labels.append(all_series[i+1][0][3])
         # if all_series[i][1][-1] < all_series[i+1][1][0]:
@@ -121,9 +119,10 @@ def create_all_labels(all_series,test_index):
     all_labels.append(all_series[-1][0][3])
     test_labels = all_labels[:-test_index]
     train_labels = all_labels[:test_index]
-    
-    print(np.shape(test_labels),np.shape(train_labels))
+
+    print(np.shape(test_labels), np.shape(train_labels))
     return test_labels, train_labels
 
-main()
 
+if __name__ == "__main__":
+    main()
