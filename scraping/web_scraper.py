82357e1bda254a0c461829bc87831cdadfdc9cfa
Michal Pluta <vsdc48@durham.ac.uk>
2024-02-08 01:32:59 +0000

    Refactor the web scraper using best practices

--- scraping/web_scraper.py
+++ scraping/web_scraper.py
@@ -1,104 +1,122 @@
-import time
-from datetime import datetime as dt
-import requests as rq
+import os
 import pandas as pd
-import numpy as np
-from dateutil import relativedelta
+import requests as rq
+from datetime import datetime as dt
+from dateutil.relativedelta import relativedelta
 
 # GLOBAL VARIABLES
-FILEPATH = 'scraping/'
-OUT_FILEPATH_ALL = FILEPATH + 'SP_data_all.csv'
-OUT_FILEPATH_SINGLE = FILEPATH + 'SP_data_single.csv'
-endDate = 0 # actual date or 'today'
-numStocks = 503 # current max = 503
+DIR = 'scraping/'
+OUT_DIR_ALL = os.path.join(DIR, 'SP_data_all_test.csv')
+OUT_DIR_SINGLE = os.path.join(DIR, 'SP_data_single.csv')
 
-# String to unix timestamp
-def getTimestamp(date):
-    dayInUnix = 86400
-    timestamp = 0
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 
-    if type(date) == int:
-        timestamp = dt.timestamp(dt.now() - relativedelta(months=date))
+COLUMNS = ['date', 'open', 'high', 'low', 'close', 'adj_close', 'volume']
+
+NUM_STOCKS = 503  # current max = 503
+
+START = 12 * 10  # 10 years of data
+END = 0  # actual date or 'today'
+
+
+def get_relative_timestamp(months: int):
+    """
+    Gets the timestamp of the date that is `months` months in the past from the current date
+    """
+    seconds_in_day = 86400
+
+    if isinstance(months, int):
+        timestamp = dt.timestamp(dt.now() - relativedelta(months=months))
     else:
-        timestamp = dt.strptime(date, '%d/%m/%Y').timestamp()
+        timestamp = dt.strptime(months, '%d/%m/%Y').timestamp()
 
-    return int(timestamp - (timestamp % dayInUnix))
+    # Ensure the timestamp is exactly midnight
+    return int(timestamp // seconds_in_day) * seconds_in_day
 
-# Tags of all the stocks in S&P 500
-def getAllTags():
-    temp_df = pd.read_csv(FILEPATH + 'SP_tags.csv')
-    return temp_df['stock'].tolist()
 
-# Convert format of date
-def changeDate(date:str):
-    dateElems = date.split("-")
-    return dateElems[2] + "/" + dateElems[1] + "/" + dateElems[0]
+def get_all_stock_tags():
+    """
+    Return the tags of all the stocks in S&P 500
+    """
+    path = os.path.join(DIR, 'SP_tags.csv')
+    df = pd.read_csv(path)
+    return df['stock'].tolist()
 
-# Initial DataFrame
-df = pd.DataFrame([], columns=['date', 'open', 'high', 'low', 'close', 'volume'], dtype = str)
 
-endDate = getTimestamp(endDate)
+def format_date(date: str):
+    """
+    Convert format of date
+    """
+    date_elems = date.split("-")
+    return f"{date_elems[2]}/{date_elems[1]}/{date_elems[0]}"
 
-def get_one(df, yearAgo, stockTag):
-    startDate = getTimestamp(yearAgo)
 
-    print(stockTag)
-    
-    # URL setup & GET request
-    my_url = f"https://query1.finance.yahoo.com/v7/finance/download/{stockTag}?period1={startDate}&period2={endDate}&interval=1d&events=history"
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-    r = rq.get(url=my_url, headers=headers)
-    
+def process_ok_response(r: rq.Response):
+    """
+    Process the response from the request
+    """
+    data = r.text
 
-    if r.status_code == 200:
-        # Get raw data from request
-        data = r.text
-        data = data.split("\n")
-        data = [day.split(",") for day in data][1:]
-        
-        # Convert raw data to DataFrame
-        temp_df = pd.DataFrame(data, columns =['date', 'open', 'high', 'low', 'close', 'adj_close', 'volume'], dtype = str)
-        
-        # Drop adjusted close column and add stock tag to the rows
-        temp_df = temp_df.drop(['adj_close'], axis=1)
-        temp_df['name'] = stockTag
+    # Split each day's data and skip the header
+    data = data.split("\n")[1:]
+    data = [day.split(",") for day in data]
 
-        # Add rows from stock to the main DataFrame
-        df = pd.concat([df, temp_df], ignore_index=True)
-        
-    # Convert format of dates
-    df['date'] = df['date'].apply(changeDate)
-    df.to_csv(OUT_FILEPATH_SINGLE, index=False)
+    # Convert raw data to DataFrame
+    df = pd.DataFrame(data, columns=COLUMNS, dtype=str)
+    df = df.drop(['adj_close'], axis=1)
 
-def get_all(df, startDate):
-    startDate = getTimestamp(startDate)
-    
-    # Scrape all the stocks
-    for idx, stock_tag in enumerate(getAllTags()[:numStocks]):
-        print(idx + 1, ':', stock_tag)
+    return df
+
+
+def fetch(tags: list[str], start: int, end: int):
+    """
+    Fetch stock data for the given tags, start, and end timestamps
+    """
+    # Calculate timestamps
+    start_ts = get_relative_timestamp(start)
+    end_ts = get_relative_timestamp(end)
+
+    # Initial DataFrame
+    df = pd.DataFrame(columns=['date', 'open', 'high', 'low', 'close', 'volume'], dtype=str)
+
+    for i, tag in enumerate(tags):
+        print(i, tag)
 
         # URL setup & GET request
-        my_url = "https://query1.finance.yahoo.com/v7/finance/download/{}?period1={}&period2={}&interval=1d&events=history".format(stock_tag, startDate, endDate)
-        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-        r = rq.get(url=my_url, headers=headers)
+        url = f"https://query1.finance.yahoo.com/v7/finance/download/{tag}?period1={start_ts}&period2={end_ts}&interval=1d&events=history"
+        r = rq.get(url=url, headers=HEADERS)
 
         if r.status_code == 200:
-            # Get raw data from request
-            data = r.text
-            data = data.split("\n")
-            data = [day.split(",") for day in data][1:]
+            # Process the response from the request
+            temp_df = process_ok_response(r)
 
-            
-            # Convert raw data to DataFrame
-            temp_df = pd.DataFrame(data, columns =['date', 'open', 'high', 'low', 'close', 'adj_close', 'volume'], dtype = str)
-            
-            # Drop adjusted close column and add stock tag to the rows
-            temp_df = temp_df.drop(['adj_close'], axis=1)
-            temp_df['name'] = stock_tag
+            # Add stock tag to each row
+            temp_df['name'] = tag
 
-
-            # Add rows from stock to the main DataFrame
+            # Add rows to the main DataFrame
             df = pd.concat([df, temp_df], ignore_index=True)
-    # Convert format of dates
-    df['date'] = df['date'].apply(changeDate)
-    df.to_csv(OUT_FILEPATH_ALL, index=False)
+
+    df['date'] = df['date'].apply(format_date)
+
+    return df
+
+
+def fetch_one(start: int, end: int, stock_tag: str):
+    """
+    Fetch stock data for a single stock tag
+    """
+    df = fetch([stock_tag], start, end)
+    df.to_csv(OUT_DIR_SINGLE, index=False)
+
+
+def fetch_all(start: int, end: int):
+    """
+    Fetch stock data for all stock tags
+    """
+    tags = get_all_stock_tags()[:NUM_STOCKS]
+    df = fetch(tags, start, end)
+    df.to_csv(OUT_DIR_ALL, index=False)
+
+
+if __name__ == '__main__':
+    fetch_all(START, END)
